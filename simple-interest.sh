9e0875c747373dac1fae2e050f2232348ee19875
Anyiam chiemelie joel <97412516+picasso5-ui@users.noreply.github.com>
2023-11-07 07:08:09 -0600

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # picasso5-ui
    # Input:
    # p, principal amount
    # t, time period in years
